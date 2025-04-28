de22b7b6611c74a4c8d442a12d7a2c80a17a8c51
Bijay Acharya <bijay.acharya@westcliff.edu>
2025-04-28 16:24:16 +0545

    deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone", // (optional if you want optimization)
+  trailingSlash: false,
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // optional if you don't use next/image
+  },
+  
+  assetPrefix: '',
+  basePath: '',
 };
 
 export default nextConfig;
